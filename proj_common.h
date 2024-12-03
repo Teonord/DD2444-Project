9a5a77d1278f5b4b49cf0bba04e78dac5a99f943
Teonord <teo.nordstrom@outlook.com>
2024-12-03 14:26:41 +0100

    Added parallel output using MPI_File. Attempted to save only position and velocity, could not.

--- proj_common.h
+++ proj_common.h
@@ -2,40 +2,23 @@
 #include <math.h>
 #include <stdio.h>
 #include <time.h>
+#include <mpi.h>
 
 #define RANDOM 1        // If seed should be randomized
-#define VERIF 0         // If Verification is to be run
+#define FILENAME "output.dat" // Shared Filename if used
 
 #define PI 3.14159265358979323846
 
-#if VERIF
-    #define TIMESTEPS 50   // Amount of Timesteps
-    #define NUMBER 25      // Amount of Birds
-    #define SEED 621        // Seed for Randomness
+#define TIMESTEPS 20   // Amount of Timesteps
+#define NUMBER 500      // Amount of Birds
+#define BIRD_DOUBLES 11 // Amount of doubles in Bird struct
 
-    #define V0 1.0          // Velocity
-    #define ETA 0.5         // Random Fluctuation in Angle (Radians)
-    #define L 4.0          // Size of Box
-    #define R_INIT 1.0      // Interaction Radius
-    #define DT 1.0          // Time Step 
+#define V0 1.0          // Velocity
+#define RFA 1.0         // Random Fluctuation in Angle (Radians)
+#define L 5.0          // Size of Box
+#define R_INIT 1.0      // Interaction Radius
+#define DT 0.05          // Time Step 
 
-    int bn = 0; /**< Global variable to keep track of bird number during verification. */
-    const double initxpos[25] = {0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4}; /**< Initial x positions for birds during verification. */
-    const double initypos[25] = {0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4}; /**< Initial y positions for birds during verification. */
-    const double initheta[25] = {0,1,2,3,4,5,6,0,1,2,3,4,5,6,0,1,2,3,4,5,6,0,1,2,3}; /**< Initial angles for birds during verification. */
-
-#else
-    #define TIMESTEPS 500   // Amount of Timesteps
-    #define NUMBER 500      // Amount of Birds
-    #define BIRD_DOUBLES 11 // Amount of doubles in Bird struct
-
-    #define V0 1.0          // Velocity
-    #define RFA 1.0         // Random Fluctuation in Angle (Radians)
-    #define L 5.0          // Size of Box
-    #define R_INIT 1.0      // Interaction Radius
-    #define DT 0.05          // Time Step 
-
-#endif
 
 #if RANDOM
     #define SEED time(NULL)
@@ -54,11 +37,11 @@ struct Bird
     double x; /**< X coordinate of the bird. */
     double y; /**< Y coordinate of the bird. */
     double z; /**< Z coordinate of the bird. */
-    double theta; /**< First angle  component of the bird. */
-    double eta; /**< Second angle  component of the bird. */
     double vx; /**< X component of velocity of the bird. */
     double vy; /**< Y component of velocity of the bird. */
     double vz; /**< Y component of velocity of the bird. */
+    double theta; /**< First angle  component of the bird. */
+    double eta; /**< Second angle  component of the bird. */
     double sint; /**< Sum of sine components of neighboring bird angles for theta. */
     double costcose; /**< Sum of cos components of neighboring bird angles for theta and eta. */
     double costsine; /**< Sum of cos components of neighboring bird angles for theta and sin components for eta. */
@@ -69,16 +52,11 @@ struct Bird
 *   Function uses the function rand() from <stdlib.h> to generate a random integer value between 0 and RAND_MAX.
 *   This value is then cast to a double-precision float and divided by RAND_MAX to get a random value between 0 and 1.
 *   Values used in numerous positions in the code.
-*   If in verification mode, always return a value of 0.55.
 *   
 * @return Random double-precision float in range [0.0, 1.0]
 */
 double randd() {
-    #if VERIF
-        return 0.55; /**< Placeholder for randomness in case of verification*/
-    #else
-        return (double)rand() / (double)RAND_MAX; /**< Generate and return a value in range [0.0, 1.0]*/
-    #endif    
+    return (double)rand() / (double)RAND_MAX; /**< Generate and return a value in range [0.0, 1.0]*/ 
 }
 
 /** @brief Performs a modulo operation that works in the proper way for negative values.
@@ -102,30 +80,18 @@ double modd(double val, double modulo) {
  * @brief Initializes the position and velocity of a bird.
  *
  * This function initializes the position and velocity of a bird struct.
- * If in verification mode, it sets the position based on predefined arrays.
- * Otherwise, it sets random positions and angles.
+ * Sets random positions and angles.
  *
  * @param b Pointer to the bird struct to be initialized.
  */
 void initBird(struct Bird *b) {
-    #if VERIF
-        #pragma omp critical
-        {
-            b->x = initxpos[bn]; /**< Initial x position of the bird. */
-            b->y = initypos[bn]; /**< Initial y position of the bird. */
+    b->x = randd() * L; /**< Random initial x position within a range. */
+    b->y = randd() * L; /**< Random initial y position within a range. */
+    b->z = randd() * L; /**< Random initial z position within a range. */
 
-            b->theta = initypos[bn]; /**< Initial angle of the bird. */
+    b->theta = PI * (randd() - 0.5); /**< Random initial first angle within -PI/2 to PI/2. */
+    b->eta = 2 * PI * (randd() - 0.5); /**< Random initial second angle within -PI to PI. */
 
-            bn++; /**< Increment the bird number. */
-        }
-    #else
-        b->x = randd() * L; /**< Random initial x position within a range. */
-        b->y = randd() * L; /**< Random initial y position within a range. */
-        b->z = randd() * L; /**< Random initial z position within a range. */
-
-        b->theta = PI * (randd() - 0.5); /**< Random initial first angle within -PI/2 to PI/2. */
-        b->eta = 2 * PI * (randd() - 0.5); /**< Random initial second angle within -PI to PI. */
-    #endif
     b->vx = V0 * cos(b->theta) * cos(b->eta); /**< Initial velocity along x direction. */
     b->vy = V0 * cos(b->theta) * sin(b->eta); /**< Initial velocity along y direction. */
     b->vz = V0 * sin(b->theta); /**< Initial velocity along z direction. */
---
+++ proj_mpi_io.c
@@ -0,0 +1,101 @@
+#include "proj_common.h"
+#include <stdio.h>
+#include <omp.h>
+#include <string.h>
+
+/**
+ * @brief Main function of the simulation.
+ *
+ * This function performs the entire simulation loop for the Vicsek model of Flocking Birds using MPI Processes and OpenMP Threads.
+ * Using functions in proj_common.h the model is solved and results are printed in one line per timestep. 
+ * The resulting values can then be pasted into the Python program to visualize the flock.
+ *
+ * @param argc Number of command-line arguments. Unused.
+ * @param argv Array of command-line argument strings. Unused.
+ * @return Returns 0 upon successful completion.
+ */
+int main(int argc, char *argv[])
+{
+    int i, j, rank, size, provided, num_pp, startnum; /**< Loop counters and MPI variables. */
+    struct Bird *b; /**< Reusable pointer to a bird struct. */
+
+    remove("output.dat");
+
+    double R = pow(R_INIT, 2); /**< Interaction radius squared, used in Pythagorean theorem. */
+
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided); /**< Initialize MPI with single thread support. */
+    MPI_Comm_size(MPI_COMM_WORLD, &size); /**< Get the total number of processes. */
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); /**< Get the rank of the current process. */
+    
+    MPI_File fh; /**< Creates a MPI IO File Reference*/
+    MPI_File_open(MPI_COMM_WORLD, FILENAME, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh); /**< Opens the MPI IO File as write only*/
+
+    if (rank == 0) {
+        if (NUMBER % size != 0) { //**< Program will only run if amount of processes is a divisor of total birds for maths reasons. */
+            printf("Please start with a process number that is a divisor of %d", NUMBER);
+            return 1;
+        }
+        printf("%d %d %f %f\n", NUMBER, TIMESTEPS, L, DT);
+    } 
+
+    num_pp = NUMBER / size; /**< Calculate the number of birds per process. */
+    startnum = rank * num_pp; /**< Calculate the starting index for birds for this process. */
+    
+    srand(SEED * rank); /**< Seed the random number generator with a unique seed for each process. */
+
+    struct Bird *birds = calloc(NUMBER, sizeof(struct Bird)); /**< Allocate memory for all birds. */
+    struct Bird *proc_birds = calloc(num_pp, sizeof(struct Bird)); /**< Allocate memory for birds of this process. */
+
+    double startTime = omp_get_wtime(); /**< Record start time of simulation. */
+
+    if (rank == 0) {
+        #pragma omp parallel for schedule(static) private(i)
+        for (i = 0; i < NUMBER; i++) { /**< Initialize birds only on process 0. */
+            initBird(&birds[i]);
+        }
+    }
+    MPI_Bcast(birds, NUMBER * BIRD_DOUBLES, MPI_DOUBLE, 0, MPI_COMM_WORLD); /**< Broadcast all bird data to all processes. */
+
+    memcpy(proc_birds, &birds[startnum], sizeof(struct Bird) * num_pp); /**< Copy the birds for this process. */
+
+    for (i = 0; i < TIMESTEPS; i++) { /**< Main simulation loop. */
+        #pragma omp parallel for schedule(static) private(j)
+        for (j = 0; j < num_pp; j++) { /**< Update positions of all birds for this process in parallel. */
+            updateBirdPos(&proc_birds[j]);
+        }
+
+        MPI_Allgather(proc_birds, num_pp * BIRD_DOUBLES, MPI_DOUBLE, birds, num_pp * BIRD_DOUBLES, MPI_DOUBLE, MPI_COMM_WORLD); /**< Gather all birds' data. */
+
+        #pragma omp parallel private(j)
+        {
+            #pragma omp for schedule(static)
+            for (j = 0; j < num_pp; j++){ /**< Calculate angle effects for all birds for this process in parallel. */
+                calculateAngleEffects(&proc_birds[j], birds, R);
+            }
+
+            #pragma omp barrier
+
+            #pragma omp for schedule(static)
+            for (j = 0; j < num_pp; j++) { /**< Update angles of all birds for this process in parallel. */
+                updateBirdAngle(&proc_birds[j]);
+            }
+        }
+
+        MPI_Offset offset = (startnum + NUMBER * i) * sizeof(struct Bird);
+        MPI_File_set_view(fh, offset, MPI_DOUBLE, MPI_DOUBLE, "native", MPI_INFO_NULL);
+        MPI_File_write_all(fh, proc_birds, num_pp * BIRD_DOUBLES, MPI_DOUBLE, MPI_STATUS_IGNORE);
+        
+    }
+    if (rank == 0) {
+        printf("Time Taken for %d Processes, %d Threads: %f", size, omp_get_max_threads(), omp_get_wtime() - startTime); /**< Print total simulation time on process 0. */
+    }
+
+    MPI_File_close(&fh);
+
+    MPI_Finalize(); /**< Finalize MPI. */
+
+    free(birds); /**< Free memory allocated for all birds. */
+    free(proc_birds); /**< Free memory allocated for birds of this process. */
+
+    return 0; /**< Return 0 to indicate successful completion. */
+}
